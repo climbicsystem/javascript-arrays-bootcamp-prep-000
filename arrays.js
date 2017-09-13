a9def4447d24af7ace7bbc9625c9ad3eabaf1a23
Joseph Vint <josephvint@me.com>
2017-09-13 22:28:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,6 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
 
-  return array.unshift(element)()
+  return array.unshift(element)
 
 }
