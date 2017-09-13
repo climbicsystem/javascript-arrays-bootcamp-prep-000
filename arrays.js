f8b8b1d3ad2cb221842016e6573a1bbd364d9ef6
Joseph Vint <josephvint@me.com>
2017-09-13 23:46:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,3 +37,7 @@ function removeElementFromBeginningOfArray(array) {
 function destructivelyRemoveElementFromEndOfArray(array) {
   return array.pop()
 }
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, -1)
+}
